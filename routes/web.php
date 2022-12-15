f75857784cc49d695da4c5b241d1fc428e3b127d
iqbalnglames <mi06514@gmail.com>
2022-12-15 21:56:01 +0700

    feat: Component Modifying

--- routes/web.php
+++ routes/web.php
@@ -15,31 +15,34 @@ use Inertia\Inertia;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/admin', function (){
-    return 'Hi admin';
-})->middleware('role:admin');
 
-Route::get('/user', function (){
-    return 'Hi User';
-})->middleware('role:user');
+// Route::get('/', function () {
+//     return Inertia::render('Welcome', [
+//         'canLogin' => Route::has('login'),
+//         'canRegister' => Route::has('register'),
+//         'laravelVersion' => Application::VERSION,
+//         'phpVersion' => PHP_VERSION,
+//     ]);
+// });
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
+// Route::middleware('auth')->group(function () {
+//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+// });
+Route::redirect('/', '/prototype/login');
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+Route::prefix('prototype')->name('prototype.')->group(function(){
+    route::get('/login', function () {
+        return Inertia::render('Prototype/Login');
+    })->name('login');
+    route::get('/register', function(){
+        return Inertia::render('Prototype/SignUp');
+    })->name('register');
 });
 
 require __DIR__.'/auth.php';
